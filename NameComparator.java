90455fdb1ea3dd0863e4c0ca1953987422a6aa9a
mulikiganesh99 <mulikiganesh99@gmail.com>
2026-03-24 10:32:15 +0530

    Delete NameComparator.java

--- NameComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class NameComparator implements Comparator<Studentinfo>
-{
-          public int compare(Studentinfo s1,Studentinfo s2)
-         {
-
-          return s1.name.compareTo(s2.name);
-          }
-}
